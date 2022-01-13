b54938469ed3f177abd2c55b2fa0b871189872ae
juanmajimenezalf <jimenez.aljua20@cadiz.salesianos.edu>
2022-01-13 18:58:43 +0100

    cambios en modelo usuario
    modificacion modelo usuario y mejora del login y home

--- gestionproyectos/nucleo/models.py
+++ gestionproyectos/nucleo/models.py
@@ -5,36 +5,18 @@ class usuario(models.Model):
     
     username=models.CharField(max_length=40)
     password=models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.username
-
-class cliente(models.Model):
-    
-    dni=models.CharField(max_length=9, unique=True)
-    nombre=models.CharField(max_length=40)
-    apellidos=models.CharField(max_length=60)
-    direccion=models.CharField(max_length=150)
-    fechaNacimiento=models.DateField()
-    fechaAlta=models.DateField(auto_now_add=True)
-    activo=models.SmallIntegerField()
-    idUsuario=models.ForeignKey(usuario, on_delete=models.CASCADE)
-
-
-    def __str__(self):
-        return self.dni
-
-class empleados(models.Model):
-   
-    dni=models.CharField(max_length=9, unique=True)
     nombre=models.CharField(max_length=40)
     apellidos=models.CharField(max_length=60)
     direccion=models.CharField(max_length=150)
     biografia=models.CharField(max_length=255)
-    idUsuario=models.ForeignKey(usuario, on_delete=models.CASCADE)
+    fechaNacimiento=models.DateField()
+    fechaAlta=models.DateField(auto_now_add=True)
+    activo=models.SmallIntegerField()
+    rol=models.CharField(max_length=20)
 
     def __str__(self):
-        return self.dni
+        return self.username
+
     
 class categorias(models.Model):
        
@@ -51,7 +33,7 @@ class proyectos(models.Model):
     nivel: models.IntegerField()
     fechainicion: models.DateField()
     fechafin: models.DateField()
-    idEmpleado: models.ForeignKey(empleados, on_delete=models.CASCADE)
+    idEmpleado: models.ForeignKey(usuario, on_delete=models.CASCADE)
     idCategoria: models.ForeignKey(categorias, on_delete=models.CASCADE)
     
     def __str__(self):
@@ -60,7 +42,7 @@ class proyectos(models.Model):
 
 class participa(models.Model):
     
-    idCliente: models.ForeignKey(cliente, on_delete=models.CASCADE)
+    idCliente: models.ForeignKey(usuario, on_delete=models.CASCADE)
     idProyecto: models.ForeignKey(proyectos, on_delete=models.CASCADE)
     fechaInscripcion: models.DateField()
     rol: models.CharField(max_length=100)
