170d7c87a7000198a3b36737a058415a0762b46a
juanmajimenezalf <jimenez.aljua20@cadiz.salesianos.edu>
2022-01-31 15:51:13 +0100

    Arreglo en modelos y login

--- gestionproyectos/nucleo/models.py
+++ gestionproyectos/nucleo/models.py
@@ -4,17 +4,17 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     
-    username=models.CharField(max_length=40,default=False, unique=True)
-    password=models.CharField(max_length=255,default=False)
-    nombre=models.CharField(max_length=40,default=False)
-    apellidos=models.CharField(max_length=60,default=False)
-    direccion=models.CharField(max_length=150,default=False)
-    biografia=models.CharField(max_length=255,default=False)
-    fechaNacimiento=models.DateField(default=False)
-    fechaAlta=models.DateField(default=False)
-    activo=models.SmallIntegerField(default=False)
-    is_cliente = models.BooleanField('cliente status', default=False)
-    is_empleado = models.BooleanField('empleado status', default=False)
+    username = models.CharField(max_length=40, unique=True)
+    password = models.CharField(max_length=255)
+    nombre = models.CharField(max_length=40)
+    apellidos = models.CharField(max_length=60)
+    direccion = models.CharField(max_length=150)
+    biografia = models.CharField(max_length=255)
+    fechaNacimiento = models.DateField()
+    fechaAlta = models.DateField(auto_now_add=True)
+    activo = models.SmallIntegerField()
+    is_cliente = models.BooleanField('cliente status')
+    is_empleado = models.BooleanField('empleado status')
 
     def __str__(self):
         return self.username
@@ -22,21 +22,21 @@ class User(AbstractUser):
     
 class categorias(models.Model):
        
-    nombre: models.CharField(max_length=150)
-    foto: models.CharField(max_length=255)
+    nombre = models.CharField(max_length=150)
+    foto = models.CharField(max_length=255)
     
     def __str__(self):
         return self.nombre
         
 class proyectos(models.Model):
     
-    titulo: models.CharField(max_length=150)
-    descripcion: models.CharField(max_length=255)
-    nivel: models.IntegerField()
-    fechainicion: models.DateField()
-    fechafin: models.DateField()
-    idEmpleado: models.ForeignKey(User, on_delete=models.CASCADE)
-    idCategoria: models.ForeignKey(categorias, on_delete=models.CASCADE)
+    titulo = models.CharField(max_length=150)
+    descripcion = models.CharField(max_length=255)
+    nivel = models.IntegerField()
+    fechainicion = models.DateField()
+    fechafin = models.DateField()
+    idEmpleado = models.ForeignKey(User, on_delete=models.CASCADE)
+    idCategoria = models.ForeignKey(categorias, on_delete=models.CASCADE)
     
     def __str__(self):
         return self.titulo
@@ -44,10 +44,10 @@ class proyectos(models.Model):
 
 class participa(models.Model):
     
-    idCliente: models.ForeignKey(User, on_delete=models.CASCADE)
-    idProyecto: models.ForeignKey(proyectos, on_delete=models.CASCADE)
-    fechaInscripcion: models.DateField()
-    rol: models.CharField(max_length=100)
+    idCliente = models.ForeignKey(User, on_delete=models.CASCADE)
+    idProyecto = models.ForeignKey(proyectos, on_delete=models.CASCADE)
+    fechaInscripcion = models.DateField()
+    rol = models.CharField(max_length=100)
     
     def __str__(self):
         return self.fechaInscripcion
--- gestionproyectos/registration/views.py
+++ gestionproyectos/registration/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.shortcuts import render
 from django.urls import reverse_lazy
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
